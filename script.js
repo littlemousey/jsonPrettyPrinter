601813b4416d4a73343ffb13e59da4bb2824750f
Ans de Nijs <ansdenijs@gmail.com>
2018-05-22 21:26:42 +0200

    Error handling and error messages added

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 function convertJSON () {
     let input = document.getElementById('input-field').value;
-    input = JSON.parse(input);
-    input = JSON.stringify(input, undefined, 4);
-    document.getElementById('output-field').innerHTML = input;
+    try {
+        input = JSON.parse(input);
+        input = JSON.stringify(input, undefined, 4);
+        document.getElementById('output-field').innerHTML = input;
+    }
+    catch(error) {
+        alert('Something seems to be wrong with the input JSON. Check the console log for more info');
+        console.log(error);
+    }
+
 
 }
 
 function copyAll () {
-    document.getElementById('output-field').select();
-    document.execCommand('copy');
-    document.getElementById('output-field').blur();
-
-    let flashMessage = document.getElementById('flash-message');
+    if (document.getElementById('output-field').value !== "") {
+        document.getElementById('output-field').select();
+        document.execCommand('copy');
+        document.getElementById('output-field').blur();
     
-    flashMessage.classList.remove('is-hidden');
-    setTimeout(function() {
-        flashMessage.classList.add('is-hidden');
-    }, 2500);
-}
-
-function showFlashMessage() {
-    let flashMessage = document.getElementById('flash-message');
-    
-    flashMessage.classList.add('is-hidden');
-    setTimeout(function() {
+        let flashMessage = document.getElementById('flash-message');
+        
         flashMessage.classList.remove('is-hidden');
-    }, 2500);
+        setTimeout(function() {
+            flashMessage.classList.add('is-hidden');
+        }, 2500);
+        
+        if (document.getElementById('error-message').classList.length < 1) {
+            document.getElementById('error-message').classList.add('is-hidden');  
+        }
+    } else {
+        document.getElementById('error-message').classList.remove('is-hidden');
+    }
+
 }
